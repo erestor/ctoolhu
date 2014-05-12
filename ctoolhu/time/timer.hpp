e3400c72eaae6d14d1c7f69d5e9ee2dd390153ae
Martin Klemsa <m.klemsa@gmail.com>
2014-05-12 14:32:21 +0200

    simplified the timer class

--- ctoolhu/time/timer.hpp
+++ ctoolhu/time/timer.hpp
@@ -10,58 +10,35 @@ namespace Ctoolhu {
 
 	namespace Time {
 
-		//tool for measuring elapsed time since clock start
+		//tool for measuring elapsed time since instantiation or manual clock start
 		template <
 			class Resolution //how accurate should the timer be - use std::chrono::(hours|minutes|seconds|milliseconds) etc.
 		>
 		class Timer {
 
-		  protected:
+		  public:
 
 			typedef Resolution duration_type;
+			typedef std::chrono::steady_clock clock_type;
 
-		  public:
-
-			Timer() = default;
-			Timer(const Timer &) = delete;
-			Timer &operator =(const Timer &) = delete;
-
-			void StartClock()
-			{
-				using namespace std::chrono;
-				_startTime = steady_clock::now();
-			}
-
-			duration_type ElapsedTime() const
-			{
-				using namespace std::chrono;
-				return duration_cast<duration_type>(steady_clock::now() - _startTime);
-			}
-
-		  private:
-
-			std::chrono::steady_clock::time_point _startTime;
-		};
-
-		//tool for measuring elapsed time since instantiation
-		template <
-			class Resolution //how accurate should the timer be - use std::chrono::(hours|minutes|seconds|milliseconds) etc.
-		>
-		class AutoTimer : Timer<Resolution> {
-
-			typedef Timer<Resolution> base_type;
-
-		  public:
-
-			AutoTimer()
+			Timer()
 			{
 				StartClock();
 			}
 
-			typename base_type::duration_type ElapsedTime() const
+			void StartClock()
 			{
-				return base_type::ElapsedTime();
+				_startTime = clock_type::now();
 			}
+
+			duration_type ElapsedTime() const
+			{
+				return std::chrono::duration_cast<duration_type>(clock_type::now() - _startTime);
+			}
+
+		  private:
+
+			clock_type::time_point _startTime;
 		};
 
 	}; //ns Time
