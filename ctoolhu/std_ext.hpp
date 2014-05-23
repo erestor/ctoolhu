7e6a4e946445c3bb0849bbdafe67c75750c95388
Martin Klemsa <m.klemsa@gmail.com>
2014-05-23 13:59:23 +0200

    added std:: namespace qualifier where appropriate

--- ctoolhu/std_ext.hpp
+++ ctoolhu/std_ext.hpp
@@ -8,52 +8,53 @@ namespace std_ext {
 	template <class Container, class Predicate>
 	bool all_of(const Container &c, const Predicate &p)
 	{
-		return std::all_of(cbegin(c), cend(c), p);
+		return std::all_of(std::cbegin(c), std::cend(c), p);
 	}
 
 	template <class Container, class Predicate>
 	bool any_of(const Container &c, const Predicate &p)
 	{
-		return std::any_of(cbegin(c), cend(c), p);
+		return std::any_of(std::cbegin(c), std::cend(c), p);
 	}
 
 	template <class Container, class Predicate>
-	void erase_if(Container &c, const Predicate &p)
+	auto erase_if(Container &c, const Predicate &p)
+		-> decltype(c.erase(remove_if(c, p), std::end(c)))
 	{
-		c.erase(remove_if(c, p), end(c));
+		return c.erase(remove_if(c, p), std::end(c));
 	}
 
 	template <class Container, class Value>
 	auto find(Container &c, const Value &v)
-		-> decltype(std::find(begin(c), end(c), v))
+		-> decltype(std::find(std::begin(c), std::end(c), v))
 	{
-		return std::find(begin(c), end(c), v);
+		return std::find(std::begin(c), std::end(c), v);
 	}
 
 	template <class Container, class Predicate>
 	auto find_if(Container &c, const Predicate &p)
-		-> decltype(std::find_if(begin(c), end(c), p))
+		-> decltype(std::find_if(std::begin(c), std::end(c), p))
 	{
-		return std::find_if(begin(c), end(c), p);
+		return std::find_if(std::begin(c), std::end(c), p);
 	}
 
 	template <class Container, class Predicate>
 	auto remove_if(Container &c, const Predicate &p)
-		-> decltype(std::remove_if(begin(c), end(c), p))
+		-> decltype(std::remove_if(std::begin(c), std::end(c), p))
 	{
-		return std::remove_if(begin(c), end(c), p);
+		return std::remove_if(std::begin(c), std::end(c), p);
 	}
 
 	template <class Container>
 	void sort(Container &c)
 	{
-		std::sort(begin(c), end(c));
+		std::sort(std::begin(c), std::end(c));
 	}
 
 	template <class Container, class Comparator>
 	void sort(Container &c, const Comparator &comp)
 	{
-		std::sort(begin(c), end(c), comp);
+		std::sort(std::begin(c), std::end(c), comp);
 	}
 }
 
